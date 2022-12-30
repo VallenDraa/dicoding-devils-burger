72c968e5db65e23ed96f747c3f35b766a3c0b3ea
VallenDraa <jestinevallendradwiputra@gmail.com>
2022-12-30 09:16:59 +0700

    improved menu slider

--- static/scripts/profile.js
+++ static/scripts/profile.js
@@ -4,6 +4,5 @@ const profile = document.querySelector(".profile");
 export default function profileInit() {
   showProfileBtn.addEventListener("click", () => {
     profile.classList.toggle("profile-showing");
-    showProfileBtn.classList.toggle("btn-show-profile-open");
   });
 }
